ead40b118394ed85d82eb1349387cac350146cba
Mandlemankiller <mandlemankiller@email.cz>
2024-03-31 23:22:13 +0200

    Initial commit

---
+++ src/main/java/cz/jeme/programu/hydrargyrum/Hydrargyrum.java
@@ -0,0 +1,14 @@
+package cz.jeme.programu.hydrargyrum;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hydrargyrum extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
